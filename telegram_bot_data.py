483a8027af9d4f3d134ec96f3ca04fe735421694
Kamil Agliev <57670971+KamilAgliev@users.noreply.github.com>
2020-04-12 21:01:47 +0300

    Add files via upload

---
+++ telegram_bot_data.py
@@ -0,0 +1,8 @@
+import random
+
+import requests
+import telegram
+from telegram import ReplyKeyboardMarkup
+
+TOKEN_FOR_TELEGRAM_BOT = "1116820230:AAHm6C00UvlPDOk-NAnT1kPgftMPuzI-CG4"
+sessionStorage = {}
