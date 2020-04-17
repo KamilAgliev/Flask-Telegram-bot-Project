74bd330094ee129cc466f202a568866fa33cfa16
Kamil Agliev <57670971+KamilAgliev@users.noreply.github.com>
2020-04-17 20:57:14 +0300

    Delete telegram_bot_data.py

--- telegram_bot_data.py
+++
@@ -1,8 +0,0 @@
-import random
-
-import requests
-import telegram
-from telegram import ReplyKeyboardMarkup
-
-TOKEN_FOR_TELEGRAM_BOT = "1116820230:AAHm6C00UvlPDOk-NAnT1kPgftMPuzI-CG4"
-sessionStorage = {}
