755bbac68f3755028d6641b053244f886011ca45
Kamil Agliev <57670971+KamilAgliev@users.noreply.github.com>
2020-04-17 20:56:30 +0300

    Delete testing.py

--- testing.py
+++
@@ -1,6 +0,0 @@
-from data.english_data import WORDS_FOR_LEARNING
-from data.questions import Question
-cnt = 1
-for section in WORDS_FOR_LEARNING.keys():
-    for les in WORDS_FOR_LEARNING[section]['themes']:
-        print(les['title'])
